3f99d23bea235240c63477c0aa5e91de34cad7c9
jhordanluyo <jhordan271992@gmail.com>
2022-09-25 10:45:29 -0500

    Initial commit

---
+++ app/src/main/java/com/creartec/googlecourse/Loop for.kt
@@ -0,0 +1,25 @@
+package com.creartec.googlecourse
+
+fun main(){
+    val names= listOf("gisvelita","jhordan","randall","marco")
+    for (name in names){
+        print(name+"----")
+        println("tiene ${name.length} letras")
+    }
+    for (item in 1..10){
+        print("$item,")
+    }
+    println()
+    for (letter in 'a'..'z'){
+        print(letter + ",")
+    }
+    println()
+    for (item in 10 downTo 1){
+        print("$item,")
+    }
+    println()
+    for (item in 10 downTo 0 step 2){
+        print("$item,")
+    }
+
+}
